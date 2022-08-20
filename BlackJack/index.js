ad2c2866952a8c19c2691afffb16e2e37438357d
Radhika Arora <aradhika880@gmail.com>
2022-08-21 02:16:43 +0530

    BlackJack

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,18 +1,27 @@
 let firstCard = 20
-let secondCard = 0
-hasBlackJack = false
-
+let secondCard = 1
 let sum = firstCard + secondCard
-console.log(sum)
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 
-if (sum < 21){ // if(false)
-    console.log("Do you want to draw a new card?")
+let messageEl = document.getElementById("message-el")
+console.log(messageEl)
+// let sumEl = document.getElementById("sum-el") OR
+let sumEl = document.querySelector("#sum-el")
+
+function startGame(){
+    sumEl.textContent = "Sum :" + sum
+    if (sum <= 20){ // if(false)
+        message = "Do you want to draw a new card?"
+    }
+    else if (sum === 21){ // if(true)
+        message = "Wohoo! You've got a Blackjack!"
+        hasBlackJack = true
+    }
+    else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
 }
-else if (sum === 21){ // if(true)
-    console.log("You've got a Blackjack!")
-    hasBlackJack = true
-}
-else {
-    console.log("You're out of the game!")
-}
-console.log(hasBlackJack)
