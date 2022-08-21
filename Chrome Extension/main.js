038e528a2ccd7131ea311be9d441d58f114c83ae
Radhika Arora <aradhika880@gmail.com>
2022-08-21 22:45:11 +0530

    commit

--- Chrome Extension/main.js
+++ Chrome Extension/main.js
@@ -8,10 +8,19 @@ const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
+// localStorage.setItem("myName", "Radhika Arora")
+
+// localStorage.getItem("myName")
+
+// localStorage.clear()
+
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value=""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     renderLeads()
 })
 
