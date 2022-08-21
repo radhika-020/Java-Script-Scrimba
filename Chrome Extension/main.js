9dd54b5795423f48e737e8e3b433d60100e62951
Radhika Arora <aradhika880@gmail.com>
2022-08-21 21:29:56 +0530

    commit

---
+++ Chrome Extension/main.js
@@ -0,0 +1,42 @@
+// function saveInput(){
+//     console.log("button Clicked!")
+// }
+
+// This can be done using addEventListener.
+let myLeads = [] 
+const inputBtn = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
+
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value=""
+    renderLeads()
+})
+
+// function renderLeads(){
+// for(let i =0; i<myLeads.length; i++){
+//     // ulEl.innerHtml += "<li>" + myLeads[i] + "</li>"
+//     // Create an element
+//     // set the Text Content
+//     // Append to UL element
+//     const li = document.createElement("li")
+//     li.textContent=myLeads[i]
+//     ulEl.append(li)
+//     }}
+
+function renderLeads(){
+    let listItems =  ""
+    for(i=0; i<myLeads.length; i++){
+        // listItems += "<li><a target='_blank' href=' " + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        listItems += `
+        <li>
+            <a target = '_blank' href = '${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
