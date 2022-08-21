9988d23ae0f7bda1163f04af2ed2ed4fbfdf30b7
Radhika Arora <aradhika880@gmail.com>
2022-08-21 18:10:10 +0530

    Commit

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,9 +1,7 @@
-let firstCard = 10
-let secondCard = 4
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 
 let messageEl = document.getElementById("message-el")
@@ -13,11 +11,37 @@ let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector("#cards-el")
 
 function startGame(){
+    // renderGame()
+    isAlive=true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard+secondCard
     renderGame()
 }
 
+function getRandomCard(){
+    // Math.floor gives us positive valuex without decimal values.
+    // math.random gives values betweeen 0.000 to 0.999...
+    // To return a range between 1 to 6, 
+    // let randomNumber = Math.floor(Math.random() * 6) + 1
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10){
+        return 10
+    }
+    else if (randomNumber === 1){
+        return 11
+    }
+    else{
+        return randomNumber
+    }
+}
+
 function renderGame(){
-    cardsEl.textContent = "Cards :" + " " + cards[0] + " " + cards[1]
+    cardsEl.textContent = "Cards :" 
+    for (let i =0; i<cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
     sumEl.textContent = "Sum :" + " " + sum
     if (sum <= 20){ // if(false)
         message = "Do you want to draw a new card?"
@@ -35,7 +59,7 @@ function renderGame(){
 
 function newCard(){
     console.log("Drawing a new card from the deck!")
-    let card = 7
+    let card = getRandomCard()
     sum += card
     cards.push(card)
     renderGame()
